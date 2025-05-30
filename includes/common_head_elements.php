790042a7fdd71f2db59679699a48b86c52a27308
adelfr2015 <adelfr2015@gmail.com>
2025-05-30 22:06:53 +0100

    fix chatbot styles

--- includes/common_head_elements.php
+++ includes/common_head_elements.php
@@ -1,6 +1,3 @@
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,1,0" />
-
 <link rel="stylesheet" href="chatbot/style.css" />
 <meta name="api-url" content="chatbot/chatbot_api.php" />
 <script src="chatbot/script.js" defer></script>
